90ba0c6e1de82e46b23f5dcb36c54b2424778ebe
ao222 <55069089+ao222@users.noreply.github.com>
2025-02-01 05:16:20 +0000

    Basic gsheet access test

--- pages/Objective Snapshot.py
+++ pages/Objective Snapshot.py
@@ -1,1 +1,14 @@
+# streamlit_app.py
 
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+# col names: Stamp	Systolic	Diastolic	Heart Rate	Glucose
+for row in df.itertuples():
+    st.write(f"{row.Stamp} glucose :{row.Glucose}:")
