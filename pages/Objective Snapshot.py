47bf7cb132ce3919b3671b85aa8e5fec5c0bd9d5
ao222 <55069089+ao222@users.noreply.github.com>
2025-02-02 18:17:14 -0600

    Update Objective Snapshot.py
    deleted import json

--- pages/Objective Snapshot.py
+++ pages/Objective Snapshot.py
@@ -2,7 +2,6 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 import datetime
-import json
 
 # Initialize Firebase using st.secrets
 if not firebase_admin._apps:
