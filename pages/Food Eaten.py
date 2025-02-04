4bc0f307a57885e04fd4b962bbfe6c5af9e4a16c
ao222 <55069089+ao222@users.noreply.github.com>
2025-02-04 13:34:25 -0600

    Update Food Eaten.py
    Called firestore client save function

--- pages/Food Eaten.py
+++ pages/Food Eaten.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+from FirestoreClient import FirestoreClient
+
+db_client = FirestoreClient()
+st.set_page_config(page_title="AO Health Tracker", page_icon="💙")
 
 st.title("Food Snapshot")
 
@@ -7,12 +11,12 @@ with st.form("input_form"):
     col1, col2 = st.columns([3, 1])  # Adjust ratio to control space allocation
     
     with col1:
-        text_input = st.text_input("Food description")
+        description = st.text_input("Food description")
     with col2:
-        numeric_input = st.number_input("Num calories", min_value=0, step=1)
+        calories = st.number_input("Num calories", min_value=0, step=1)
     
     submit_button = st.form_submit_button("Submit")
 
 if submit_button:
-    st.write(f"Text: {text_input}")
-    st.write(f"Number: {numeric_input}")
+    db_client.save_foods_snapshot(description,calories)
+    
