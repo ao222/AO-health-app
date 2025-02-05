ff9d200c86376f5701c1098a52920bdcbc5d1218
ao222 <55069089+ao222@users.noreply.github.com>
2025-02-05 10:15:41 -0600

    Update Daily Snapshot.py
    Added caffeine form element.

--- pages/Daily Snapshot.py
+++ pages/Daily Snapshot.py
@@ -18,7 +18,8 @@ if data is None:
             "naps": 0.0,
             "walking_minutes": 0,
             "lifting_minutes": 0,
-            "calories": 0
+            "calories": 0,
+            "caffeine": 0
     }
 with st.form("daily_snapshot"):
     sleep_hours = st.number_input("Sleep (hours)", min_value=0.0, max_value=24.0, step=0.5, value=data["sleep_hours"])
@@ -26,9 +27,10 @@ with st.form("daily_snapshot"):
     walking_minutes = st.number_input("Walking (minutes)", min_value=0, step=5, value=data["walking_minutes"])
     lifting_minutes = st.number_input("Lifting (minutes)", min_value=0, step=5, value=data["lifting_minutes"])
     calories = st.number_input("Calories", min_value=0, step=50, value=data["calories"])
+    caffeine = st.number_input("Caffeine", min_value=0, step=5, value=data["caffeine"])
     
     submitted = st.form_submit_button("Submit")
     
     if submitted:
-        db_client.save_daily_snapshot(sleep_hours, naps, walking_minutes, lifting_minutes, calories)
+        db_client.save_daily_snapshot(sleep_hours, naps, walking_minutes, lifting_minutes, calories, caffeine)
         st.success("Data saved successfully!")
