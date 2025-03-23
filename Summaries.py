c5f49722548a9c898ce3c79e64505d032d1d6475
ao222 <55069089+ao222@users.noreply.github.com>
2025-03-23 06:24:06 -0500

    Update Summaries.py
    Added place holder for data summaries.

--- Summaries.py
+++ Summaries.py
@@ -7,27 +7,9 @@ import util_time
 def main():
     st.set_page_config(page_title="AO Health Tracker", page_icon="💙")
     db_client = FirestoreClient()
-    st.title("Blood Pressure Data")
+    st.title("Data Summaries")
     
-    # Date range selection
-    start_date = st.date_input("Start Date", datetime.today())
-    end_date = st.date_input("End Date", datetime.today())
-
-    if st.button("Query Data"):
-        # Convert dates to timestamps
-        # start_timestamp = datetime.combine(start_date, datetime.min.time())
-        # end_timestamp = datetime.combine(end_date, datetime.max.time())
-        start_timestamp = util_time.begin_day(start_date)
-        end_timestamp = util_time.end_day(end_date)
-        objective_df = db_client.get_objective_snapshots(start_timestamp,end_timestamp)
-
-        # Display results
-        if objective_df is not None:
-            # add column of local timezones from UTC column
-            objective_df["Local Timestamp"] = objective_df["Timestamp"].apply(lambda x: util_time.convert_to_local(x))
-            st.dataframe(objective_df)
-        else:
-            st.write("No blood pressure data found in the selected range.")
+    st.markdown("Summaries to go here. . .")
 
 if __name__ == "__main__":
     main()
