f3f6d8a1fc2fa6f4229a0934f3e78f9955863d5c
ao222 <55069089+ao222@users.noreply.github.com>
2025-03-23 05:26:07 -0500

    Update Objective Snapshot.py
    Added call to util.time_get_time()

--- pages/Objective Snapshot.py
+++ pages/Objective Snapshot.py
@@ -36,7 +36,8 @@ if today_df is not None:
     for index, row in today_df.iterrows():
         col1, col2, col3 = st.columns([3, 1, 1])  # adjust layout
         with col1:
-            st.markdown(f"**{row['systolic']} / {row['diastolic']} - hr: {row['heart_rate']} - glucose: {row['glucose']} @ {row['timestamp']}**")
+            time = util_time.get_time(row['timestamp'])
+            st.markdown(f"**{row['systolic']} / {row['diastolic']} - hr: {row['heart_rate']} - glucose: {row['glucose']} @ {time}**")
         with col2:
             if col2.button("Delete", key=f"delete_{row['timestamp']}"):
                 db_client.delete_objective_snapshot(row['timestamp'])
